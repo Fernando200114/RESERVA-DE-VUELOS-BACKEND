66becbab7f13272aac1d738d87d3c735474469c0
Ubuntu <fernando@vm-vuelos-api.01nehsonjugufbyj44rhzu2mqa.phxx.internal.cloudapp.net>
2025-12-21 18:13:21 +0000

    Sincronizando configuracion VPS

--- reservas/settings.py
+++ reservas/settings.py
@@ -27,9 +27,9 @@ load_dotenv(ENV_PATH)
 SECRET_KEY = 'django-insecure-&h7qe-q2n3_ag8!f$pgp8_h1wvc#^7ke7w#mpbder8ubq5-!4('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -132,7 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
